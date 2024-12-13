9c309a6591d436af774cf1139f1087f67f109d34
admin <admin@Veena-Manjunath.mshome.net>
2024-12-13 17:30:21 +0530

    Added new file to local branch

---
+++ ComcastCRMGUIFramwork/src/main/java/com/comCast/crm/objectRepository/CreateDashboardPage.java
@@ -0,0 +1,18 @@
+package com.comCast.crm.objectRepository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CreateDashboardPage
+{
+	public CreateDashboardPage(WebDriver driver)
+	{
+		//this.driver=driver;
+		PageFactory.initElements(driver,this);
+	}
+	@FindBy(name="lastname")
+	private WebElement lastnametxtfield;
+
+}
